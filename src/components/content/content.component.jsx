5192a0ce36763b0412ff8c5721d4cf4025437b12
Roman Muzikantov <12068940+LilCurly@users.noreply.github.com>
2020-04-29 13:41:09 +0200

    adding shop page

--- src/components/content/content.component.jsx
+++ src/components/content/content.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export const Content = (props) => {
+export const Content = ({ title }) => {
     return (
         <div className="content">
-            <h1 className="title">{ props.title }</h1>
+            <h1 className="title">{ title.toUpperCase() }</h1>
             <span className ="subtitle">SHOP NOW</span>
         </div>
     );
