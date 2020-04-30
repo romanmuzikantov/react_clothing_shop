f63a04e4794cc13473aca5127d411bbe7b3b22a5
Roman Muzikantov <12068940+LilCurly@users.noreply.github.com>
2020-04-30 14:12:56 +0200

    adding sign in/sign out component to header

--- src/App.js
+++ src/App.js
@@ -17,10 +17,24 @@ class App extends React.Component {
     }
   }
 
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
