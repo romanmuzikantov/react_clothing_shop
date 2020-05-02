a93c8df8b90361806e00244f5ea340ae78889687
Roman Muzikantov <12068940+LilCurly@users.noreply.github.com>
2020-05-02 12:04:50 +0200

    using reselect for memoization when using selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 
 import { ReactComponent as ShopingIcon } from '../../assets/cart.svg';
 import { SetHiddenState } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ setHiddenState }) => (
+const CartIcon = ({ setHiddenState, totalItem }) => (
     <div className="cart-icon" onClick={setHiddenState}>
         <ShopingIcon className="shopping-icon" />
-        <span className="item-count">0</span>
+        <span className="item-count">{totalItem}</span>
     </div>
 )
 
@@ -17,4 +18,8 @@ const mapDispatchToProps = dispatch => ({
     setHiddenState: () => dispatch(SetHiddenState())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state) => ({
+    totalItem: selectCartItemsCount(state)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
