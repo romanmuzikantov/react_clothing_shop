81da850188b8549bbd59e6f98bee97d58b4593de
Roman Muzikantov <12068940+LilCurly@users.noreply.github.com>
2020-05-02 12:30:20 +0200

    basic checkout page + redirect to checkout page from dropdown

---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './checkout.styles.scss';
+
+const CheckoutPage = () => (
+    <div class="checkout-page">
+        <h1>CHECkOUT PAGE</h1>
+    </div>
+)
+
+export default CheckoutPage;
