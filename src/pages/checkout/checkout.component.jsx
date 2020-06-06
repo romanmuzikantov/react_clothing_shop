fd3fe84b353d531670b4de17e6fda83d17638db8
Roman Muzikantov <12068940+LilCurly@users.noreply.github.com>
2020-06-06 16:36:38 +0200

    integrating stripe test payment

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,6 +6,7 @@ import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import './checkout.styles.scss';
+import StripeButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckoutPage = ({ cartItems, total }) => (
     <div className="checkout-page">
@@ -34,6 +35,7 @@ const CheckoutPage = ({ cartItems, total }) => (
         <div className="total">
             <span>TOTAL: {total}$</span>
         </div>
+        <StripeButton price={total} />
     </div>
 )
 
